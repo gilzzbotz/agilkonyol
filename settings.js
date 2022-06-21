0e196d34795611beed27614c9f7a10aad36a07f6
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-06-21 21:22:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.owner = ['6285730794089']
+global.premium = ['6285730794089']
+global.ownernomer = '6285730794089'
+global.ownername = 'GilzzOFC'
+global.botname = 'GilzzMD'
+global.footer = '©GilzzXz'
+global.ig = 'https://github.com/gilzzbotz'
+global.region = 'Jawa'
+global.sc = 'wa.me/6285730794089'
+global.myweb = ''
+global.packname = 'GilzzXz'
+global.author = 'GilzAja'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
